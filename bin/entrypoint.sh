d3626976886614accf3f69bc6cc2fa067396e4ec
Andy Potanin <andy@udx.io>
2019-05-27 14:57:47 -0400

    - added support for setting kubernetes ca cert via [KUBERNETES_CLUSTER_CERTIFICATE] env var

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -12,17 +12,22 @@ if [[ ! -f "/etc/ssh/ssh_host_dsa_key" ]]; then
   chmod 0600 /etc/ssh/ssh_host_dsa_key
 fi
 
-chmod 0600 /etc/ssh/ssh_host_ecdsa_key
+if [[ -f "/etc/ssh/ssh_host_dsa_key" ]]; then
+  chmod 0600 /etc/ssh/ssh_host_ecdsa_key
+fi;
 
-pm2 startOrReload /opt/sources/rabbitci/rabbit-ssh/static/ecosystem.config.js
+if [[ "${KUBERNETES_CLUSTER_CERTIFICATE}" != "" ]]; then
+  echo "Writing Kubernetes certificate to [/home/node/.kube/kuberentes-ca.crt]";
+  echo -e ${KUBERNETES_CLUSTER_CERTIFICATE} > /home/node/.kube/kuberentes-ca.crt
+fi;
 
-if [[ -f /opt/sources/rabbitci/rabbit-ssh/static/kuberentes-ca.crt ]]; then
-  echo "Setting up Kubernetes.";
+if [[ -f /home/node/.kube/kuberentes-ca.crt ]]; then
+  echo "Setting up Kubernetes [$KUBERNETES_CLUSTER_NAME] cluster with [$KUBERNETES_CLUSTER_NAMESPACE] namespace.";
 
   kubectl config set-cluster ${KUBERNETES_CLUSTER_NAME} \
     --embed-certs=true \
     --server=${KUBERNETES_CLUSTER_ENDPOINT} \
-    --certificate-authority=/opt/sources/rabbitci/rabbit-ssh/static/kuberentes-ca.crt
+    --certificate-authority=/home/node/.kube/kuberentes-ca.crt
 
   kubectl config set-context ${KUBERNETES_CLUSTER_NAMESPACE} \
     --namespace=${KUBERNETES_CLUSTER_NAMESPACE} \
@@ -37,6 +42,8 @@ if [[ -f /opt/sources/rabbitci/rabbit-ssh/static/kuberentes-ca.crt ]]; then
   chown node:node /home/node/.kube/config
 
 fi;
+
+pm2 startOrReload /opt/sources/rabbitci/rabbit-ssh/static/ecosystem.config.js
 
 ## Command pass-through.
 exec "$@"
--- readme.md
+++ readme.md
@@ -9,3 +9,17 @@ Run for debug:
 ```
 docker-compose up --build --renew-anon-volumes
 ```
+
+### Environment Variables
+
+* GITHUB_ACCESS_TOKENS - comma separated list of GitHub personal access tokens to use to discover repository ACL
+* KUBERNETES_CLUSTER_ENDPOINT
+* KUBERNETES_CLUSTER_NAME
+* KUBERNETES_CLUSTER_SERVICEACCOUNT
+* KUBERNETES_CLUSTER_CERTIFICATE - stringified PEM certificate
+* KUBERNETES_CLUSTER_NAMESPACE
+* KUBERNETES_CLUSTER_USER_SECRET
+* KUBERNETES_CLUSTER_USER_TOKEN
+* KUBERNETES_CLUSTER_CONTEXT
+* SLACK_NOTIFICACTION_URL
+* SLACK_NOTIFICACTION_CHANNEL

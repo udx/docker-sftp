52dbf70bb5a66b54ea44bde77649a1aaa67c710e
Andy Potanin <andy@udx.io>
2025-01-20 06:24:06 -0500

    fix: improve test environment handling

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -16,32 +16,34 @@ if [[ -f "/etc/ssh/ssh_host_dsa_key" ]]; then
   chmod 0600 /etc/ssh/ssh_host_ecdsa_key
 fi;
 
-if [[ "${KUBERNETES_CLUSTER_CERTIFICATE}" != "" ]]; then
-  echo "Writing Kubernetes certificate to [/home/node/.kube/kuberentes-ca.crt]";
-  cat /var/run/secrets/kubernetes.io/serviceaccount/ca.crt > /home/node/.kube/kuberentes-ca.crt
-fi;
+# Only setup Kubernetes if enabled
+if [[ "${SERVICE_ENABLE_K8S}" != "false" ]]; then
+  if [[ "${KUBERNETES_CLUSTER_CERTIFICATE}" != "" ]]; then
+    echo "Writing Kubernetes certificate to [/home/node/.kube/kuberentes-ca.crt]";
+    cat /var/run/secrets/kubernetes.io/serviceaccount/ca.crt > /home/node/.kube/kuberentes-ca.crt
+  fi;
 
-if [[ -f /home/node/.kube/kuberentes-ca.crt ]]; then
-  echo "Setting up Kubernetes [$KUBERNETES_CLUSTER_NAME] cluster with [$KUBERNETES_CLUSTER_NAMESPACE] namespace.";
+  if [[ -f /home/node/.kube/kuberentes-ca.crt ]]; then
+    echo "Setting up Kubernetes [$KUBERNETES_CLUSTER_NAME] cluster with [$KUBERNETES_CLUSTER_NAMESPACE] namespace.";
 
-  kubectl config set-cluster ${KUBERNETES_CLUSTER_NAME} \
-    --embed-certs=true \
-    --server=${KUBERNETES_CLUSTER_ENDPOINT} \
-    --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+    kubectl config set-cluster ${KUBERNETES_CLUSTER_NAME} \
+      --embed-certs=true \
+      --server=${KUBERNETES_CLUSTER_ENDPOINT} \
+      --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
 
-  kubectl config set-context ${KUBERNETES_CLUSTER_NAMESPACE} \
-    --namespace=${KUBERNETES_CLUSTER_NAMESPACE} \
-    --cluster=${KUBERNETES_CLUSTER_NAME} \
-    --user=${KUBERNETES_CLUSTER_SERVICEACCOUNT}
+    kubectl config set-context ${KUBERNETES_CLUSTER_NAMESPACE} \
+      --namespace=${KUBERNETES_CLUSTER_NAMESPACE} \
+      --cluster=${KUBERNETES_CLUSTER_NAME} \
+      --user=${KUBERNETES_CLUSTER_SERVICEACCOUNT}
 
-  kubectl config set-credentials ${KUBERNETES_CLUSTER_SERVICEACCOUNT} --token=${KUBERNETES_CLUSTER_USER_TOKEN}
+    kubectl config set-credentials ${KUBERNETES_CLUSTER_SERVICEACCOUNT} --token=${KUBERNETES_CLUSTER_USER_TOKEN}
 
-  kubectl config use-context ${KUBERNETES_CLUSTER_NAMESPACE}
+    kubectl config use-context ${KUBERNETES_CLUSTER_NAMESPACE}
 
-  cp /root/.kube/config /home/node/.kube/config
+    cp /root/.kube/config /home/node/.kube/config
 
-  chown -R node:node /home/node/.kube
-
+    chown -R node:node /home/node/.kube
+  fi;
 fi;
 
 # Install dependencies
